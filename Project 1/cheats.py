1d4211ac19609b19d4795324a1cfdefc7985eb3f
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-08-13 18:24:33 +0530

    start

---
+++ Project 1/cheats.py
@@ -0,0 +1,12 @@
+# open cry.csv and print all lines
+
+# import csv
+# with open('cry.csv', 'r') as cry:
+#     cry_reader = csv.reader(cry)
+#     a = {line[0]:line[1] for line in cry_reader}
+#     # print(a)
+# count = 0
+# states_dict = {}
+
+a = input()
+print(len(a))
